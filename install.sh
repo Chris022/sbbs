08350ca599c6d0cfdf215cb78e48f1216e930362
Christoph <christoph02.cw@gmail.com>
2024-10-02 11:37:27 +0200

    avoid overwriting of config file on reinstall

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-install -d /usr/bin /etc/systemd/system /usr/share/doc/sbbs
+install -d /usr/bin /etc/systemd/system /usr/share/doc/sbbs /usr/share/sbbs/
 install -Dm755 src/sbbs /usr/bin
-install -m755 src/config /etc/sbbs
+cp -n src/config /etc/sbbs
+chmod 577 /etc/sbbs
 install -Dm644 README.md /usr/share/doc/sbbs
 install -Dm644 sbbs.service sbbs.timer /etc/systemd/system
